cb5fea44dcb293637ce3e2083e4c150b6d902c5b
JunE17 <55774099+kdjidkr@users.noreply.github.com>
2024-09-26 18:51:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -82,3 +82,19 @@ function calculateTotal() {
 
     document.getElementById("totalAmount").innerText = totalAmount.toLocaleString();
 }
+
+document.documentElement.addEventListener('touchstart', function (event) {
+     if (event.touches.length > 1) {
+          event.preventDefault(); 
+        } 
+    }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+     var now = (new Date()).getTime();
+     if (now - lastTouchEnd <= 300) {
+          event.preventDefault(); 
+        } lastTouchEnd = now; 
+    }, false);
+    
